f633f382a9370fd20103e9620ad943ede003cbaf
jp3droribeiro <joaoribeiroc3po@gmail.com>
2024-10-05 23:33:04 -0300

    add functions

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -3,3 +3,71 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from io import BytesIO
 
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, root, key):
+        if root is None:
+            return Node(key)
+        if key < root.key:
+            root.left = self._insert(root.left, key)
+        elif key > root.key:
+            root.right = self._insert(root.right, key)
+        return root
+
+    def inorder_traversal(self):
+        result = []
+        self._inorder_traversal(self.root, result)
+        return result
+
+    def _inorder_traversal(self, root, result):
+        if root:
+            self._inorder_traversal(root.left, result)
+            result.append(root.key)
+            self._inorder_traversal(root.right, result)
+
+    def postorder_traversal(self):
+        result = []
+        self._postorder_traversal(self.root, result)
+        return result
+
+    def _postorder_traversal(self, root, result):
+        if root:
+            self._postorder_traversal(root.left, result)
+            self._postorder_traversal(root.right, result)
+            result.append(root.key)
+
+    def preorder_traversal(self):
+        result = []
+        self._preorder_traversal(self.root, result)
+        return result
+
+    def _preorder_traversal(self, root, result):
+        if root:
+            result.append(root.key)
+            self._preorder_traversal(root.left, result)
+            self._preorder_traversal(root.right, result)
+
+    def breadth_first_traversal(self):
+        result = []
+        queue = [self.root]
+        while queue:
+            node = queue.pop(0)
+            if node:
+                result.append(node.key)
+                queue.append(node.left)
+                queue.append(node.right)
+        return result
+    
+##

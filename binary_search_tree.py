3b11fbac6969045f18cdc614f99e9a0f0391ae03
jp3droribeiro <joaoribeiroc3po@gmail.com>
2024-10-06 21:16:16 -0300

    update

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -10,6 +10,7 @@ class Node:
         self.left = None
         self.right = None
 
+
 class BinarySearchTree:
     def __init__(self):
         self.root = None
@@ -69,13 +70,13 @@ class BinarySearchTree:
                 queue.append(node.left)
                 queue.append(node.right)
         return result
-    
-
 
     def plot_tree(self):
-        plt.figure(figsize=(12, 6))
+        # adjust the size
+        num_nodes = self.count_nodes(self.root)
+        plt.figure(figsize=(max(8, num_nodes * 0.5), max(6, num_nodes * 0.3)))
 
-
+        # Subplot
         plt.subplot(1, 2, 1)
         graph = nx.Graph()
         self._build_tree_graph(graph, self.root)
@@ -83,28 +84,40 @@ class BinarySearchTree:
         labels = {node: node.key for node in graph.nodes()}
         nx.draw(graph, pos, labels=labels, with_labels=True, node_size=700, node_color="skyblue", font_size=8)
         plt.title('Árvore de Busca Binária')
-
         
+
+        plt.axis('off') 
+    
         plt.subplot(1, 2, 2)
         in_order = self.inorder_traversal()
         post_order = self.postorder_traversal()
         pre_order = self.preorder_traversal()
         breadth_first = self.breadth_first_traversal()
 
-        plt.text(0.5, 0.9, 'Em-ordem: ' + str(in_order), ha='center', va='center', fontsize=10, bbox=dict(facecolor='red', alpha=0.5), transform=plt.gca().transAxes)
-        plt.text(0.5, 0.8, 'Pós-ordem: ' + str(post_order), ha='center', va='center', fontsize=10, bbox=dict(facecolor='green', alpha=0.5), transform=plt.gca().transAxes)
-        plt.text(0.5, 0.7, 'Pré-ordem: ' + str(pre_order), ha='center', va='center', fontsize=10, bbox=dict(facecolor='yellow', alpha=0.5), transform=plt.gca().transAxes)
-        plt.text(0.5, 0.6, 'Largura: ' + str(breadth_first), ha='center', va='center', fontsize=10, bbox=dict(facecolor='white', alpha=0.5), transform=plt.gca().transAxes)
+        plt.text(0.5, 0.9, 'Em-ordem: ' + str(in_order), ha='center', va='center', fontsize=10, bbox=dict(alpha=0.5), transform=plt.gca().transAxes)
+        plt.text(0.5, 0.8, 'Pós-ordem: ' + str(post_order), ha='center', va='center', fontsize=10, bbox=dict(alpha=0.5), transform=plt.gca().transAxes)
+        plt.text(0.5, 0.7, 'Pré-ordem: ' + str(pre_order), ha='center', va='center', fontsize=10, bbox=dict( alpha=0.5), transform=plt.gca().transAxes)
+        plt.text(0.5, 0.6, 'Largura: ' + str(breadth_first), ha='center', va='center', fontsize=10, bbox=dict( alpha=0.5), transform=plt.gca().transAxes)
         plt.title('Resultados dos Percursos')
+        
+        # Remover os eixos
+        
+        plt.axis('off')  # Oculta os eixos x e y
 
         plt.tight_layout()
 
-    # CREATE  A BUFFER TO SAVE FIGURE
-        buf = BytesIO() 
+        buf = BytesIO()
         plt.savefig(buf, format="png")
         buf.seek(0)
+        plt.close()  # Fecha a figura após salvar
         return buf
-    
+
+    def count_nodes(self, root):
+        """Contabiliza o número de nós na árvore."""
+        if root is None:
+            return 0
+        return 1 + self.count_nodes(root.left) + self.count_nodes(root.right)
+
     def _build_tree_graph(self, graph, root):
         if root:
             graph.add_node(root)
@@ -130,34 +143,32 @@ class BinarySearchTree:
                 queue.append((node.right, depth + 1, offset + 1))
 
         return pos
-    
 
-# Interface do Streamlit
+
+# streamlit interface
 
 st.set_page_config(
     page_title="Árvore de Busca Binária",
-    page_icon=" ",
-    layout="centered",
+    page_icon="2",
     initial_sidebar_state='expanded',
-    menu_items={
-        'About': ''
-    }
+    
 )
+
 st.title("Árvore de Busca Binária")
 
 input_sequence = st.text_input("Digite a sequência de números separados por espaço:")
 submit = st.button("Inserir na Árvore")
 
 if submit:
-    
     elements = list(map(int, input_sequence.split()))
     arvore = BinarySearchTree()
     
     for element in elements:
         arvore.insert(element)
-    
+
     st.write("Árvore gerada:")
     buf = arvore.plot_tree()
     st.image(buf)
 
 
+##1
